f4b9f1df3bee2afbb2c95614238f10506e94cfaa
Shubham Yerawar <shubhamyerawar8@gmail.com>
2018-04-17 18:20:00 +0530

    test with ngrok added

---
+++ test_ngrok/dev-server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { handler } = require('./../lambda/src/index.js');
+
+const server = express();
+server.use(bodyParser.json());
+
+// Create POST route
+server.post('/', (req, res) => {
+    // Create dummy context with fail and succeed functions
+    const context = {
+        fail: () => res.sendStatus(500),
+        succeed: data => res.send(data),
+    };
+
+    // Initialize alexa sdk
+    handler(req.body, context);
+});
+
+// Start express server
+server.listen(3000, () => {
+    console.log('Local alexa skill listening on port 3000!');
+});
